16402e86987faa91b3ecc7c78305acdf85077481
Sean Whalen <whalenster@gmail.com>
2013-08-02 00:15:23 +0000

    0.4.1 take II

--- dcnotify/scraper.py
+++ dcnotify/scraper.py
@@ -19,24 +19,28 @@ from bs4 import BeautifulSoup as bs
 __author__ = "Sean Whalen"
 __copyright__ = "Copyright (C) 2012 %s" % __author__
 __license__ = "MIT"
-__version__ = "1.2.0"
+__version__ = "1.3.0"
 
 BASE_URL = "http://dragoncon.org"
 
 
 def _get_soup(path):
     """Gets soup from the given path, respecting robots.txt"""
-    user_agent = 'dcnotify/%s' % __version__
-    http_headers = {'User-Agent': '%s' % user_agent}
+
     full_path = BASE_URL + path
 
+    # Set a user-agent
+    user_agent = 'dcnotify/%s' % __version__
+    http_headers = {'User-Agent': '%s' % user_agent}
+
+    # Honor robots.txt
     robots = RobotFileParser()
     robots.set_url("%s/robots.txt" % BASE_URL)
     robots.read()
-
     if not robots.can_fetch(user_agent, full_path):
         raise ValueError("Path disallowed by robots.txt")
 
+    # Make a make a request, raising any HTTP errors that might occur
     request = get(full_path, headers=http_headers)
     request.raise_for_status()
 
@@ -45,28 +49,43 @@ def _get_soup(path):
 
 def get_guests():
     """ Returns a list of dictionaries containing guest data"""
-    # regex false-positive
-    # pylint: disable=W1401
 
+    # The path to the guest list
     guest_path = "/?q=guests"
+    
+    # A CSS-style selector for guest listings
     guest_selector = "div.member-prof-pros p"
 
+    # Get the page content
     soup = _get_soup(guest_path)
 
+    # Remove all script and style elements
     for elem in soup.findAll(['script', 'style']):
         elem.extract()
 
+    # Find all the guest listings
     raw_guests = soup.select(guest_selector)
 
     guests = []
 
+    # Parse the guest listings
     for guest in raw_guests:
+<<<<<<< HEAD
+        if len(guest.text) > 0:
+    	    name = unicode(guest.a.get_text())
+            url = "%s/%s" % (BASE_URL, guest.a['href'])
+            id_ = int(match(r".*/(\d*)", url).group(1)) # Use existing PK
+            guest.a.extract() # Remove the name, leaving only the description
+            description = guest.get_text().strip().lstrip(u"\xbb ") # Remove extra chars
+            new_guest = dict(id=id_, name=name, url=url, description=description)
+=======
         name = unicode(guest.a.get_text())
         url = "%s/%s" % (BASE_URL, guest.a['href'])
-        id_ = int(match(".*/(\d*)", url).group(1))
-        guest.a.extract()
-        description = guest.get_text().strip().lstrip(u"\xbb ")
+        id_ = int(match(".*/(\d*)", url).group(1)) # Use existing PK
+        guest.a.extract() # Remove the name, leaving only the description
+        description = guest.get_text().strip().lstrip(u"\xbb ") # Remove extra chars
         new_guest = dict(id=id_, name=name, url=url, description=description)
+>>>>>>> b3ee216a1c27b18c1790a2fd07fa77107d5c6fa7
 
         guests.append(new_guest)
 
